746ae3aaa73fb98fc6f652ac7989a1d5e1489cb8
Makin Ahmed <ahmedmakin555@gmail.com>
2021-05-19 19:45:53 +0600

    added simple jokes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {   useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [joke, setJoke] = useState("At first type your name below!");
+
+  const handleJoke = (firstName, lastName) => {
+    fetch(
+      `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
+    )
+      .then((res) => res.json())
+      .then((res2) => {
+        setJoke(res2.value.joke);
+      });
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Make Yourself Laugh!!</h2>
+      <p>{joke}</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <label htmlFor="firstName">
+        <h4>Type First Name: </h4>
+      </label>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <label htmlFor="lastName">
+        <h4>Type Last Name: </h4>
+      </label>
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={() => handleJoke(firstName, lastName)}>
+        {" "}
+        Submit Name
+      </button>
     </div>
   );
 }
